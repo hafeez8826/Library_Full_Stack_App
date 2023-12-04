b924f49418ff90e1003abfa8b2699492befd92bb
hafeez <dhz939127@gmail.com>
2023-12-04 21:58:24 +0530

    Home Page - react

---
+++ 03-frontend/library/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import HomePage from "./layouts/HomePage/HomePage";
+import Footer from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+
---
+++ 03-frontend/library/src/layouts/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+
+import ExploreTopBooks from "./components/ExploreTopBooks";
+import Carousel from "./components/Carousel";
+import Heros from "./components/Heros";
+import LibraryServices from "./components/LibraryServices";
+
+const HomePage = () => {
+  return (
+    <>
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+      <LibraryServices />
+    </>
+  );
+};
+
+export default HomePage;
