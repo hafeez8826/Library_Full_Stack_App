5a6018d420baa827e7036e1ef95e1268ae90f18f
hafeez <dhz939127@gmail.com>
2023-12-26 15:34:43 +0530

    search book component implemented

--- 03-frontend/library/src/App.tsx
+++ 03-frontend/library/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import HomePage from "./layouts/HomePage/HomePage";
 import Footer from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import SearchBookPage from "./layouts/SearchBookPage/SearchBookPage";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBookPage />
       <Footer />
     </div>
   );
