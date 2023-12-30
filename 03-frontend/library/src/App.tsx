87948c417998f2f84e1bec1d042c6a3e4e4f7cbb
hafeez <dhz939127@gmail.com>
2023-12-30 12:59:54 +0530

    added login and logout functionaliy
    home page refactoring

--- 03-frontend/library/src/App.tsx
+++ 03-frontend/library/src/App.tsx
@@ -1,32 +1,56 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import "./App.css";
 import HomePage from "./layouts/HomePage/HomePage";
 import Footer from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import SearchBookPage from "./layouts/SearchBookPage/SearchBookPage";
 import BookCheckoutPage from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import oktaConfig from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { LoginCallback, Security } from "@okta/okta-react";
+import LoginWidget from "./Auth/LoginWidget";
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+  const customAuthHandler = () => {
+    history.push("/login");
+  };
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
+
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <div className="flex-grow-1">
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/search">
-            <SearchBookPage />
-          </Route>
-          <Route path="/checkout/:bookId">
-            <BookCheckoutPage />
-          </Route>
-        </Switch>
-      </div>
-      <Footer />
+    <div className='d-flex flex-column min-vh-100'>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+        <Navbar />
+        <div className="flex-grow-1">
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path="/search">
+              <SearchBookPage />
+            </Route>
+            <Route path="/checkout/:bookId">
+              <BookCheckoutPage />
+            </Route>
+          <Route path='/login' render={
+            () => <LoginWidget config={oktaConfig} /> 
+            }
+            />
+            <Route path="/login/callback" component={LoginCallback} />
+          </Switch>
+        </div>
+        <Footer />
+      </Security>
     </div>
   );
 };
--- 03-frontend/library/src/layouts/HomePage/components/Heros.tsx
+++ 03-frontend/library/src/layouts/HomePage/components/Heros.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
 
 const Heros = () => {
+  const { authState } = useOktaAuth();
+
   return (
     <div>
       <div className="d-none d-lg-block">
@@ -16,9 +19,19 @@ const Heros = () => {
                 Whether it is to learn a new skill or grow within one, we will
                 be able to provide the top content for you!
               </p>
-              <a className="btn main-color btn-lg text-white" href="#">
-                Sign up
-              </a>
+              {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="/search"
+                >
+                  Explore top books
+                </Link>
+              ) : (
+                <Link className="btn main-color btn-lg text-white" to="/login">
+                  Sign up
+                </Link>
+              )}
             </div>
           </div>
         </div>
@@ -51,9 +64,19 @@ const Heros = () => {
                 Whether it is to learn a new skill or grow within one, we will
                 be able to provide the top content for you!
               </p>
-              <a className="btn main-color btn-lg text-white" href="#">
-                Sign up
-              </a>
+              {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="/search"
+                >
+                  Explore top books
+                </Link>
+              ) : (
+                <Link className="btn main-color btn-lg text-white" to="/login">
+                  Sign up
+                </Link>
+              )}
             </div>
           </div>
           <div className="m-2">
