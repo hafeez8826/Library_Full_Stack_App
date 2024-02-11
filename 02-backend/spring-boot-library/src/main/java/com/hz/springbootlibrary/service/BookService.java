526811986321be93151ba600b786a64f9df536d3
hafeez <dhz939127@gmail.com>
2024-02-11 11:38:27 +0530

    set up for shelf page

--- 02-backend/spring-boot-library/src/main/java/com/hz/springbootlibrary/service/BookService.java
+++ 02-backend/spring-boot-library/src/main/java/com/hz/springbootlibrary/service/BookService.java
@@ -5,13 +5,19 @@ import com.hz.springbootlibrary.dao.CheckoutRepository;
 
 import com.hz.springbootlibrary.entity.Book;
 import com.hz.springbootlibrary.entity.Checkout;
+import com.hz.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
 import org.hibernate.annotations.Check;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -59,6 +65,69 @@ public class BookService {
 
         return checkoutRepository.findBooksByUserEmail(userEmail).size();
     }
+
+    public List<ShelfCurrentLoansResponse> currentLoans(String userEmail) throws Exception{
+        List<ShelfCurrentLoansResponse> shelfCurrentLoansResponses = new ArrayList<>();
+        List<Checkout> checkoutList = checkoutRepository.findBooksByUserEmail(userEmail);
+
+        List<Long> bookIdList = new ArrayList<>();
+
+        for(Checkout i : checkoutList){
+            bookIdList.add(i.getBookId());
+        }
+
+        List<Book> books = bookRepository.findBookByBookIds(bookIdList);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        for(Book book : books){
+            Optional<Checkout> checkout = checkoutList.stream().
+                    filter(x -> x.getBookId() == book.getId()).findFirst();
+            if(checkout.isPresent()){
+                Date d1 = sdf.parse(checkout.get().getReturnDate());
+                Date d2 = sdf.parse(LocalDate.now().toString());
+
+                TimeUnit timeUnit = TimeUnit.DAYS;
+
+                long difference_In_Time = timeUnit.convert(d1.getTime() - d2.getTime(),
+                        TimeUnit.MILLISECONDS);
+
+                shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) difference_In_Time));
+            }
+        }
+
+        return shelfCurrentLoansResponses;
+    }
+
+    public void returnBook(String userEmail, Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout == null){
+            throw new Exception("Book does not exists, or not checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        bookRepository.save(book.get());
+        checkoutRepository.deleteById(validateCheckout.getId());
+    }
+
+    public void renewLoan(String userEmail, Long bookId) throws Exception{
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if(validateCheckout == null){
+            throw new Exception("Book does not exists or not checkout by user");
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 = sdf.parse(validateCheckout.getReturnDate());
+        Date d2 = sdf.parse(LocalDate.now().toString());
+
+        if(d1.compareTo(d1) > 0 || d1.compareTo(d2) == 0){
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(validateCheckout);
+        }
+    }
+
 
 
 
--- 03-frontend/library/src/App.tsx
+++ 03-frontend/library/src/App.tsx
@@ -7,9 +7,10 @@ import SearchBookPage from "./layouts/SearchBookPage/SearchBookPage";
 import BookCheckoutPage from "./layouts/BookCheckoutPage/BookCheckoutPage";
 import oktaConfig from "./lib/oktaConfig";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
-import { LoginCallback, Security } from "@okta/okta-react";
+import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import ReviewListPage from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
+import ShelfPage from "./layouts/ShelfPage/ShelfPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -51,6 +52,7 @@ export const App = () => {
             }
             />
             <Route path="/login/callback" component={LoginCallback} />
+            <SecureRoute path="/shelf"> <ShelfPage /> </SecureRoute>
           </Switch>
         </div>
         <Footer />
